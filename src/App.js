7d8798a561d15203705db99782364fdbe2b5385f
ahmedelhdad <106435040+ahmedelhdad@users.noreply.github.com>
2024-03-12 01:35:09 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -3,35 +3,35 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import { MainContainer, CreateContainer, Header } from './Components/ControlWebsite'
 import { AnimatePresence } from 'framer-motion'
 import { useDispatch } from 'react-redux'
-import {FOOT_ITEMS} from './Redux toolkit/Slice/Slice'
-import {getAllFoodItems} from './Utils/firebaseFunctions'
+import { FOOT_ITEMS } from './Redux toolkit/Slice/Slice'
+import { getAllFoodItems } from './Utils/firebaseFunctions'
 import { useState } from 'react'
+
 const App = () => {
-  const [isCart,setIsCart] = useState() 
+  const [isCart, setIsCart] = useState()
   const dispatch = useDispatch()
-  const fetchData = async() => 
-  {
-    await getAllFoodItems().then((data) => dispatch(FOOT_ITEMS(data)))
-  } 
+
   useEffect(() => {
-    
-    return () => fetchData()
-  },[])
-  const HandlerFun = (e) => 
-  {
+    const fetchData = async () => {
+      await getAllFoodItems().then((data) => dispatch(FOOT_ITEMS(data)))
+    }
+    fetchData()
+  }, [dispatch])
+  const HandlerFun = (e) => {
     setIsCart(e)
   }
-  // console.log('App')
+
+
   return (
     <BrowserRouter >
-      <Header HandlerFun= {HandlerFun} />
+      <Header HandlerFun={HandlerFun} />
       <AnimatePresence mode='wait'>
         <main className='pt-20 container'>
-        <Routes>
-          <Route path='/*' element={<MainContainer isCart={isCart} HandlerFun= {HandlerFun} />} />
-          <Route path='createItem' element={<CreateContainer />} />
+          <Routes>
+            <Route path='/*' element={<MainContainer isCart={isCart} HandlerFun={HandlerFun} />} />
+            <Route path='createItem' element={<CreateContainer />} />
 
-        </Routes>
+          </Routes>
         </main>
       </AnimatePresence>
     </BrowserRouter>
